07160928911b8aee1902d88b831d5e14339c6628
Daniel Podwysocki <daniel@contact24.pl>
2021-04-11 03:11:35 +0200

    add functionality for creating security groups

--- plugins/modules/proxmox-sg.py
+++ plugins/modules/proxmox-sg.py
@@ -2,18 +2,21 @@
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
+import proxmoxer
+from proxmoxer import ProxmoxAPI
+import requests
 
 DOCUMENTATION = r'''
 ---
-module: my_test
+module: proxmox-sg
 
-short_description: This is my test module
+short_description: This is a module to create security groups to be used with the proxmox firewall
 
 # If this is part of a collection, you need to use semantic versioning,
 # i.e. the version is of the form "2.5.0" and not "2.4".
 version_added: "1.0.0"
 
-description: This is my longer description explaining my test module.
+description: Placeholder.
 
 options:
     name:
@@ -68,3 +71,77 @@ message:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        name=dict(type='str', required=True),
+        comment=dict(type='str', required=False),
+        api_password=dict(type='str', required=True, no_log=True),
+        api_user=dict(type='str', required=True),
+        api_host=dict(type='str', required=True),
+        verify_ssl=dict(type='bool', required=False, default=True)
+
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['original_message'] = module.params['name']
+    result['message'] = 'goodbye'
+
+    #determine if the group already exists
+    proxmox = ProxmoxAPI(module.params['api_host'], user=module.params['api_user'],
+                     password=module.params['api_password'], verify_ssl=module.params['verify_ssl'])
+    security_groups = proxmox.cluster.firewall.groups.get()    
+    
+    #check if the sg exists, if it does set sg_exists to True
+    sg_exists=False
+    for sg in security_groups:
+        if sg['group'] == module.params['name']:
+           sg_exists = True
+
+
+    #the result defaults to False, if we're creating a group, make it True and then create the group
+    if not sg_exists:
+        result['changed'] = True
+        proxmox.cluster.firewall.groups.create(group=module.params['name'])
+
+
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
